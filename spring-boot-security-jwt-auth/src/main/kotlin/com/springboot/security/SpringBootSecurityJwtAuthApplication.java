1893cf207156b04ca1d6c7c46b8c281802f9cf9c
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-10-14 12:48:12 +0530

    spring-boot-security-jwt-auth project added

---
+++ spring-boot-security-jwt-auth/src/main/kotlin/com/springboot/security/SpringBootSecurityJwtAuthApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author prabhakar, @Date 05-10-2024
+ */
+@SpringBootApplication
+public class SpringBootSecurityJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityJwtAuthApplication.class, args);
+        System.out.println("Application Running Successfully...@");
+    }
+}
---
+++ spring-boot-security-jwt-auth/src/main/kotlin/com/springboot/security/SpringBootSecurityJwtAuthApplication.kt
@@ -0,0 +1,12 @@
+//package com.springboot.security
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.boot.runApplication
+//
+//@SpringBootApplication
+//class SpringBootSecurityJwtAuthApplication
+//
+//fun main(args: Array<String>) {
+//	runApplication<SpringBootSecurityJwtAuthApplication>(*args)
+//	println("Application Running...@")
+//}
